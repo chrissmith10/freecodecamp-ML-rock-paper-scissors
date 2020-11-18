471da30d0776c8ab99525d4951044d2bc1542cac
chrissmith10 <52174925+chrissmith10@users.noreply.github.com>
2020-11-18 15:39:42 -0500

    Create RPS.py

---
+++ RPS.py
@@ -0,0 +1,28 @@
+
+def player(prev_play, opponent_history=[]):
+    opponent_history.append(prev_play)
+
+
+    # need to have program actually learn. I want to collect data. I want my program to learn some pattern from that data. Then my program should develop a winning strategy (60+% win rate) and play that until the game ends.
+
+    # 1) Collect Data
+    # 2) Learn From Data
+    # 3) Develop Winning Strategy (60+% win rate)
+    # 4) Execute
+
+    # 1)
+    # collectData=[]
+    # I think I can just use the opponent_history list
+
+    # 2)
+    # learnBabylearn
+
+    # 3)
+    # 13/20 * 100 > 60
+    # or do I want it to check itself every 8 data, 125 times? in that case, we want to make sure my program wins 5/8 games
+
+    # 4)
+    # time_to_win
+
+
+    return guess
